af3f448942284204850150854e3aa87b29e94fcf
Slamchillz <mendyslam@gmail.com>
2022-12-21 17:20:29 +0000

    Throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,10 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(String(error));
+  }
+  queue.push('Guardrail was processed');
+	return queue;
+}
