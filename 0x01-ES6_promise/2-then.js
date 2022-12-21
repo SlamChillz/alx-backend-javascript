48248c52c80d0f219d52700da503bc2691a49cc8
Slamchillz <mendyslam@gmail.com>
2022-12-21 17:49:27 +0000

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
-    .then(() => console.log('Got a response from the API'));
+    .finally(console.log('Got a response from the API'));
 }
