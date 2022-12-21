1bbf8c610e3a78fec0e36882ea40c5c3dde221df
Slamchillz <mendyslam@gmail.com>
2022-12-21 19:42:24 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((array) => array);
+    .then((results) => {
+      results.map((result) => ({
+        status: result.status,
+        value: (result.status === 'fulfilled' ? result.value : String(result.reason)),
+      }));
+      return results;
+    });
 }
