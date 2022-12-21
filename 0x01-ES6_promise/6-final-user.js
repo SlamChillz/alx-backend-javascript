f73b96bb510fde179829cc3ce75067ccfce9fbbe
Slamchillz <mendyslam@gmail.com>
2022-12-21 16:26:45 +0000

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((array) => array);
+}
