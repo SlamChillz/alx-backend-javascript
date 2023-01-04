6379aec71d2f77bdb350296f40384b621e14056b
slamchillz <mendyslam@gmail.com>
2023-01-04 01:19:55 +0100

    fix: clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
-  let array = Array.from(set);
-  const len = startString.length;
-  array = array.filter((a) => (a[0] === startString[0]) && (a.substring(0, len) === startString));
-  return array.map((s) => s.substring(len)).join('-');
+  if ((set instanceof Set) && (typeof startString === 'string')) {
+    let array = Array.from(set);
+    const len = startString.length;
+    array = array.filter((a) => (typeof a === 'string') && (a[0] === startString[0]) && (a.substring(0, len) === startString));
+    return array.map((s) => s.substring(len)).join('-');
+  }
+  return '';
 }
