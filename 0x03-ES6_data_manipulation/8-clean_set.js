a0ac8311be57b87291a0287ccde944f673780ff2
slamchillz <mendyslam@gmail.com>
2023-02-21 10:41:19 +0100

    print to STDOUT

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if ((set instanceof Set) && (typeof startString === 'string')) {
     let array = Array.from(set);
     const len = startString.length;
-    array = array.filter((a) => (typeof a === 'string') && (a[0] === startString[0]) && (a.substring(0, len) === startString));
+    array = array.filter((a) => typeof a === 'string' && a[0] === startString[0] && a.startsWith(startString));
     return array.map((s) => s.substring(len)).join('-');
   }
   return '';
