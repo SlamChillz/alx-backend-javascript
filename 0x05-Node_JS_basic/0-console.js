93a23614af002f4dfe24ab677fc7b9b613216ab7
slamchillz <mendyslam@gmail.com>
2023-02-21 11:04:35 +0100

    added new line to stdout data

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,7 +2,7 @@
  * Prints a string to the standard output
  */
 function displayMessage(message) {
-  process.stdout.write(message);
+  process.stdout.write(`${message}\n`);
 }
 
 module.exports = displayMessage;
