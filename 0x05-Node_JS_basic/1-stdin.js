7322fd2b7a0480299e48d818592b7f09ef15d3e2
slamchillz <mendyslam@gmail.com>
2023-02-21 11:53:30 +0100

    using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+/**
+ * Reads stdin and writes to stdout
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
