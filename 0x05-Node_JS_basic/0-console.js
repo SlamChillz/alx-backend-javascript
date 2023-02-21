861d154f30f2bb17ded6f242013683363ffbeebc
slamchillz <mendyslam@gmail.com>
2023-02-21 10:48:10 +0100

    print to STDOUT

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+/*
+ * Prints a string to the standard output
+ */
+function displayMessage(message) { /* eslint-disable-line no-unused-vars */
+  process.stdout.write(message);
+}
+
+module.exports = displayMessage;
