feddfa0725d7080d4e1d5851f1cae524d78a96b2
slamchillz <mendyslam@gmail.com>
2023-02-24 17:52:28 +0100

    stubbing test technique

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,35 @@
+/**
+ * Test that payment called Utils.calculateNumber
+ */
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToAp', function() {
+  beforeEach(function() {
+    sinon.spy(console, "log");
+    sinon.stub(Utils, 'calculateNumber').returns(10);
+  });
+  afterEach(function() {
+    sinon.restore();
+  });
+  it("validate the usage of the `Utils.calculateNumber` function", function() {
+    sendPaymentRequestToApi(100, 20);
+    // Check it is called
+    expect(Utils.calculateNumber.called).to.be.true;
+    // Check it is called once
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+    // Check call count
+    expect(Utils.calculateNumber.callCount).to.be.equal(1);
+    // Check it first argument
+    expect(Utils.calculateNumber.firstCall.args[0]).to.equal("SUM");
+    // Check params
+    expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    // Check console call count
+    expect(console.log.callCount).to.be.equal(1);
+    // Check console param
+    expect(console.log.calledWith('The total is: 10')).to.be.true;
+  });
+});
