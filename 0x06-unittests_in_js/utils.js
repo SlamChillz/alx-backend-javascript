7cdb1501d2c925b974b1307f1b126fe2c1cbe534
slamchillz <mendyslam@gmail.com>
2023-02-24 17:06:27 +0100

    utilities module

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,22 @@
+/**
+ * Defines utilities
+ */
+const Utils = {
+  calculateNumber: function(type, a, b) {
+    let result;
+    if (type === 'SUM') {
+      result = Math.round(a) + Math.round(b);
+    } else if (type === 'SUBTRACT') {
+      result = Math.round(a) - Math.round(b);
+    } else if (type === 'DIVIDE') {
+      if (Math.abs(Math.round(b)) === 0) {
+        result = 'Error';
+      } else {
+        result = Math.round(a) / Math.round(b);
+      }
+    }
+    return result;
+  },
+}
+
+module.exports = Utils;
